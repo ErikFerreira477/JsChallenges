c6addd9b6cd34b06f824607265a47fcbdff7e66b
ErikFerreira477 <erikcalcada@hotmail.com>
2022-06-18 23:08:22 -0300

    JavascriptBasics

---
+++ JavaScriptBasics.js
@@ -0,0 +1,59 @@
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/sum-two-numbers-javascript
+const sumTwoNumbers = (num1, num2) => {
+    return (num1 + num2);
+};
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/comparison-strict-equality
+const comparisonOperators = (value1, value2) => {
+    if (value1 === value2 && typeof value1 === typeof val2) {
+        return true;
+    }
+
+    return false;
+};
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/type-value-javascript
+const getTypeOfValue = (value) => {
+    return typeof value;
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/get-nth-character-string-javascript
+const getCharOfString = (string, position) => {
+    return (string.charAt(position));
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/remove-first-characters-string-javascript
+const removeFirstNChars = (string, quantity) => {
+    return (string.slice(quantity));
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/get-last-characters-string-javascript
+const getLastThreeChars = (string) => {
+    return (string.slice(-3));
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/get-first-characters-string-javascript
+const getFirstThreeChars = (string) => {
+    return (string.slice(0, 3));
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/extract-first-half-string-javascript
+const getFirstHalfOfString = (string) => {
+    return (string.slice(0, string.length / 2));
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/extract-first-half-string-javascript
+const removeLastThreeCharOfString = (string) => {
+    return (string.slice(0, string.length - 3));
+}
+
+//https://www.jschallenger.com/javascript-fundamentals/javascript-basics/return-percentage-of-number
+// const = () => {
+
+// }
+
+const percentageOf = (value1, value2) => {
+    return ((value2 / value1) * 100).toFixed(2)
+}
+
+console.log(percentageOf(10, 1))
